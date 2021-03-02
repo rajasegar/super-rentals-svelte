cd83d405e74710ed6d85912d7794e09f7011f3a8
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-03-02 08:41:23 +0530

    chore: first commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,12 @@
 <script>
   import {onMount} from 'svelte';
+  import { Router, link, Route } from 'svelte-routing';
+  import Home from './routes/Home.svelte';
+  import About from './routes/About.svelte';
+  import Contact from './routes/Contact.svelte';
+
+  export let url = "";
+
   let count = 0;
   onMount(() => {
     const interval = setInterval(() => count++, 1000);
@@ -10,60 +17,81 @@
 </script>
 
 <style>
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
   :global(body) {
     margin: 0;
     font-family: Arial, Helvetica, sans-serif;
   }
-  .App {
-    text-align: center;
-  }
-  .App code {
-    background: #0002;
-    padding: 4px 8px;
-    border-radius: 4px;
-  }
-  .App p {
-    margin: 0.4rem;
+
+  .container {
+    max-width: 1024px;
+    min-height: 100vh;
+    background: #f9f9f9;
+    margin: 0 auto;
   }
 
-  .App-header {
-    background-color: #f9f6f6;
-    color: #333;
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    font-size: calc(10px + 2vmin);
+  .menu {
+    height: 4em;
+    background-color: #e46855;
   }
-  .App-link {
-    color: #ff3e00;
-  }
-  .App-logo {
-    height: 36vmin;
-    pointer-events: none;
-    margin-bottom: 3rem;
-    animation: App-logo-pulse infinite 1.6s ease-in-out alternate;
-  }
-  @keyframes App-logo-pulse {
-    from {
-      transform: scale(1);
-    }
-    to {
-      transform: scale(1.06);
-    }
+
+  .menu h1 {
+  position: relative;
+  padding: 5px 0 0 8px;
+  color: #f9f9f9;
+  font-size: 1.8em;
+  font-style: italic;
+}
+
+a, .links {
+  display: inline-block;
+}
+
+ a {
+  text-decoration: none;
+  padding: 0 15px;
+  color: #fff;
+  font-size: 20px;
+  font-weight: bold;
+}
+
+ a:hover,
+ a.active {
+  opacity: 1;
+}
+
+ .links {
+  padding: 0 21px;
+}
+
+ .links a {
+  position: relative;
+  bottom: 5px;
+}
+
+
+  .body {
+    padding: 15px;
   }
 </style>
 
-<div class="App">
-  <header class="App-header">
-    <img src="/logo.svg" class="App-logo" alt="logo" />
-    <p>Edit <code>src/App.svelte</code> and save to reload.</p>
-    <p>Page has been open for <code>{count}</code> seconds.</p>
-    <p>
-      <a class="App-link" href="https://svelte.dev" target="_blank" rel="noopener noreferrer">
-        Learn Svelte
-      </a>
-    </p>
-  </header>
+<div class="container">
+<Router url="{url}">
+  <nav class="menu">
+    <a href="/" use:link><h1>Super Rentals</h1></a>
+      <div class="links">
+    <a  href="/about" use:link>About</a>
+    <a href="/contact" use:link>Contact</a>
+      </div>
+  </nav>
+  <div class="body">
+    <Route path="/about" component="{About}"/>
+    <Route path="/contact" component="{Contact}"/>
+    <Route path="/" component="{Home}"/>
+  </div>
+</Router>
 </div>
---
+++ src/routes/Home.svelte
@@ -0,0 +1,8 @@
+<style>
+</style>
+<div class="jumbo">
+  <div class="right svelte"></div>
+  <h2>Welcome to Super Rentals</h2>
+  <p>We hope you find exactly what you're looking for in a place to stay.</p>
+  <a href="/about" use:link>About Us</a>
+</div>
